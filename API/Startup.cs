ca2227b4712a64e9cc8a54a12fa11600aab7c742
Parker <jwk839928622@outlook.com>
2021-01-23 12:23:31 +0800

    configure Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,8 @@ namespace API
       {  //GetConnectionString
         options.UseSqlite(_configuration["ConnectionStrings:DefaultConnection"]);
       });
+
+      services.AddCors();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +58,9 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
-
+      app.UseCors(options => {
+          options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+      });
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
