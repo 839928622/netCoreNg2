96c2c62cd8b7e0715acc0a0a015963f8132a7dbf
alex <839928622@qq.com>
2021-03-10 10:22:29 +0800

    confirm service  translation

--- client/src/app/services/confirm.service.ts
+++ client/src/app/services/confirm.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
@@ -8,9 +10,24 @@ import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.
 })
 export class ConfirmService {
   bsModelRef: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  title: string; message: string; btnOkText: string; btnCancelText: string;
+
+  constructor(private modalService: BsModalService, private translate: TranslateService) {
+    this.translate.stream('confirm-service.title').pipe(take(1)).subscribe((title: string) => {
+      this.title = title;
+    });
+    this.translate.stream('confirm-service.message').pipe(take(1)).subscribe((message: string) => {
+      this.message = message;
+    });
+    this.translate.stream('confirm-service.btnOkText').pipe(take(1)).subscribe((btnOkText: string) => {
+      this.btnOkText = btnOkText;
+    });
+    this.translate.stream('confirm-service.btnCancelText').pipe(take(1)).subscribe((btnCancelText: string) => {
+      this.btnOkText = btnCancelText;
+    });
+  }
   // tslint:disable-next-line: max-line-length
-  confirm(title = 'Confirmation', message= 'Are you sure you wannt to do this?', btnOkText = 'OK', btnCancelText = 'Cancel'): Observable<boolean>
+  confirm(title = this.title, message= this.message, btnOkText = this.btnOkText, btnCancelText = this.btnCancelText): Observable<boolean>
   {
     // just like we use modal to modify user's roles
     // we came up with some configuration
